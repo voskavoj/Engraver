f969079e42017d02294881b23579811f71c514bb
Voska Vojtech <vojtech.voska@comap-control.com>
2023-08-20 22:10:04 +0200

    WIP thresholding

--- src/image_processing.py
+++ src/image_processing.py
@@ -2,9 +2,6 @@ from PIL import Image, ImageOps
 import cv2
 import numpy as np
 
-from logging import info as log
-from logging import warning as warn
-
 
 def _open_and_resize_image(filename, len_x, res_x):
     image = Image.open(filename)
@@ -14,10 +11,10 @@ def _open_and_resize_image(filename, len_x, res_x):
     # resize
     size_x = round(len_x * res_x)
     size_y = round(size_x * 1 / aspect_ratio)
-    log(size_x, size_y)
+    print(size_x, size_y)
 
     if size_x > org_size_x or size_y > org_size_y:
-        warn("New image is larger")
+        print("WARNING: New image is larger")
 
     image = image.resize((size_x, size_y))
 
@@ -32,13 +29,23 @@ def process_image_for_drawing(filename, len_x, res_x, max_pwr, min_pwr, **kwargs
     image = ImageOps.grayscale(image)
     image = ImageOps.invert(image)
 
+    # Threshold
+    threshold = (10, 245)
+    image_pixels = image.load()
+    for i in range(size_x):
+        for j in range(size_y):
+            if image_pixels[i, j] < threshold[0]:
+                image_pixels[i, j] = threshold[0]
+            elif image_pixels[i, j] > threshold[1]:
+                image_pixels[i, j] = threshold[1]
+
     # map to min, max power
     image_pixels = image.load()
     for i in range(size_x):
         for j in range(size_y):
             val = image_pixels[i, j]
             # normalized value = (value / OLD_MAX) * NEW_MAX - SHIFT, where NEW_MAX is from 0
-            image_pixels[i, j] = round(((val / 255) * (max_pwr - min_pwr)) + min_pwr)
+            image_pixels[i, j] = round(((val / threshold[1]) * (max_pwr - min_pwr)) + min_pwr)
 
     return image
 
@@ -49,13 +56,22 @@ def process_image_for_cut(filename, len_x, res_x, **kwargs):
     image = np.asarray(image)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
+    # Threshold
+    threshold = (10, 245)
+    for i in range(297):
+        for j in range(300):
+            if image[i][j] < threshold[0]:
+                image[i][j] = threshold[0]
+            elif image[i][j] > threshold[1]:
+                image[i][j] = threshold[1]
+
     # Find Canny edges
     edged = cv2.Canny(image, 30, 200)
 
     # Finding Contours
     contours, _ = cv2.findContours(edged, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-    log("Number of Contours found = " + str(len(contours)))
+    print("Number of Contours found = " + str(len(contours)))
     # for c in contours:
     #     np.savetxt("foo" + str(len(c)) + ".csv", c[:,0,:], delimiter=";")
 
